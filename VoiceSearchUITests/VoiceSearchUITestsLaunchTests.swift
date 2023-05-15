d7870ec177a632ef5809c838c06051a162f200dc
Jorge Roberto <jorgercj@dcomp.ufs.br>
2023-05-15 15:56:19 -0300

    First Commit

---
+++ VoiceSearchUITests/VoiceSearchUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  VoiceSearchUITestsLaunchTests.swift
+//  VoiceSearchUITests
+//
+//  Created by Jorge de Carvalho on 15/05/23.
+//
+
+import XCTest
+
+final class VoiceSearchUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
